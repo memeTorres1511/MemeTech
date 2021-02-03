0522799fb5b93b3780fae1310aee149eb31d828a
Manuel Garcia <meme.torres.1511@gmail.com>
2021-02-02 22:31:50 -0600

    archivo js daniado

---
+++ js/lightbox.js
@@ -0,0 +1,9 @@
+const imagenes1 = document.querySelectorAll('.img-galeria')
+const imagenesLight = document.querySelector('.agregar-imagen')
+const contenedorLight = document.querySelector('.imagen-light')
+
+imagenes1.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        alert('auch')
+    })
+})
