10fb84841a09cc717b3330b703dc54a9f060e1be
Manuel Garcia <meme.torres.1511@gmail.com>
2021-02-26 02:19:11 -0600

    implementacion del footer y cursos

--- js/sliderNew.js
+++ js/sliderNew.js
@@ -1,8 +1,8 @@
-let btnMenu = document.getElementById('btn-menu');
+/* let btnMenu = document.getElementById('btn-menu');
 let mainNav = document.getElementById('main-nav');
 btnMenu.addEventListener('click', function(){
   mainNav.classList.toggle('mostrar');
-});
+}); */
 
 const slider = document.querySelector("#slider");
 let sliderSection = document.querySelectorAll(".slider__section");
